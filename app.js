3f64cabb66f78e836d95b5a2801c323d8d5c28e2
Jonatas <jonatasjc_@hotmail.com>
2019-06-22 15:04:00 -0300

    Test ok

--- app.js
+++ app.js
@@ -5,9 +5,60 @@ const path = require('path');
 
 const app = express();
 
-// Static
+// Bodyparser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Signup Route
+app.post('/signup', (req, res) => {
+  const { firstName, lastName, email } = req.body;
+
+  // Make sure fields are filled
+  if (!firstName || !lastName || !email) {
+    res.redirect('/fail.html');
+    return;
+  }
+
+  // Construct req data
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const postData = JSON.stringify(data);
+
+  const options = {
+    url: 'https://us3.api.mailchimp.com/3.0/lists/3f08e2fbc0',
+    method: 'POST',
+    headers: {
+      Authorization: 'auth f9ed49efd3e997448e7e7ced0c4277d6-us3'
+    },
+    body: postData
+  };
+
+  request(options, (err, response, body) => {
+    if (err) {
+      res.redirect('/fail.html');
+    } else {
+      if (response.statusCode === 200) {
+        res.redirect('/success.html');
+      } else {
+        res.redirect('/fail.html');
+      }
+    }
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('Server Started oN ${PORT}'));
+app.listen(PORT, console.log(`Server started on ${PORT}`));
